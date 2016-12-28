0e5cf82363ab180980472c4df1a30828e3ca772c
Todd Suess <todd@datacomponents.net>
2016-12-27 19:19:08 -0500

    Memo widget is now scrollable
    Building out pieces of Notepad example for demonstration
    purposes as well as to determine best methods to implement for
    most commonly used widgets.

--- example_notepad.py
+++ example_notepad.py
@@ -1,8 +1,10 @@
 from form import Form
+from dialogs import OpenDialog, SaveDialog
 from memo import Memo
 from statusbar import StatusBar
 from menu import MainMenu
 import os
+import sys
 
 class NotepadForm(Form):
     def __init__(self, filename=None):
@@ -10,20 +12,29 @@ class NotepadForm(Form):
         self._set_caption(filename)
         self._editor = Memo(self, left=0, top=0, width=self.width, height=self.height)
         self._status = StatusBar(self)
-        self._menu = self._create_menubar()
+        self._create_menubar()
 
     def _file_new(self):
         pass
+
+    def _file_open(self):
+        dlg = OpenDialog(self)
+        if dlg.execute():
+            self._editor.lines.open_file(dlg.filename)
+            self._set_caption(dlg.filename)
+
+    def _file_exit(self):
+        sys.exit(0)
 
     def _create_menubar(self):
         menu = MainMenu(self)
         with menu.create('File') as file_menu:
             file_menu.create('New', on_click=self._file_new)
-            file_menu.create('Open')
+            file_menu.create('Open', on_click=self._file_open)
             file_menu.create('Save')
             file_menu.create('Save As...')
             file_menu.separator()
-            file_menu.create('Exit')
+            file_menu.create('Exit', on_click=self._file_exit)
 
     def _set_caption(self, filename):
         if not filename:
--- memo.py
+++ memo.py
@@ -1,11 +1,57 @@
 from control import Control
+from containers import ScrollContainer
 from Tkinter import Text, END
 
-class Memo(Control):
+class _StringListWrapper(object):
+    def __init__(self, ctrl):
+        self._ctrl = ctrl
+
+    @property
+    def count(self):
+        return int(self._ctrl.index('end').split('.')[0])
+
+    def __getitem__(self, index):
+        return self._get_line(index)
+
+    def __setitem__(self, index, value):
+        self._set_line(index, value)
+
+    def _get_line(self, index):
+        # TODO: check index
+        # NOTE: Tkinter considers '1' the first line
+        index = int(index)+1
+        return self._ctrl.get("{}.0".format(index), "{}.end".format(index))
+
+    def _set_line(self, index, value):
+        # TODO: check index
+        index = int(index)+1
+        self._ctrl.delete("{}.0".format(index), "{}.end".format(index))
+        self._ctrl.insert("{}.0".format(index), value)
+
+    def clear(self):
+        self._ctrl.delete("1.0", END)
+
+    def open_file(self, filename):
+        with open(filename, 'r') as fp:
+            self.clear()
+            for line in fp:
+                self._ctrl.insert(END, line)
+
+    def save_file(self, filename):
+        with open(filename, 'w') as fp:
+            fp.write(self._ctrl.get("1.0", END))
+
+
+class BaseMemo(Control):
     def __init__(self, parent, **kwargs):
         Control.__init__(self, Text(master=parent._frame), **kwargs)
+        self._line_wrapper = _StringListWrapper(self._ctrl)
         if kwargs.get('text'):
             self._ctrl.insert(END, kwargs['text'])
+
+    @property
+    def lines(self):
+        return self._line_wrapper
 
     @property
     def text(self):
@@ -13,6 +59,22 @@ class Memo(Control):
 
     @text.setter
     def text(self, value):
-        value = str(value)
         self._ctrl.delete(1.0, END)
-        self._ctrl.insert(END, value)
+        self._ctrl.insert(END, str(value))
+
+class Memo(ScrollContainer):
+    def __init__(self, parent, **kwargs):
+        ScrollContainer.__init__(self, parent, **kwargs)
+        self._init_container(BaseMemo(self, **kwargs))
+
+    @property
+    def lines(self):
+        return self._container.lines
+
+    @property
+    def text(self):
+        return self._container.text
+
+    @text.setter
+    def text(self, value):
+        self._container.text = value
