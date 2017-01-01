07532bc73ce4a82f4b8d5de4399477c1d71a3bd8
Todd Suess <todd@datacomponents.net>
2016-12-31 23:27:51 -0500

    Adding canvas property

--- paintbox.py
+++ paintbox.py
@@ -6,3 +6,7 @@ class PaintBox(Control):
     def __init__(self, parent, **kwargs):
         Control.__init__(self, TkCanvas(parent._frame), **kwargs)
         self._canvas = Canvas(self._ctrl)
+
+    @property
+    def canvas(self):
+        return self._canvas
